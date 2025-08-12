93b4971825f574f6c1c903d5513bd811d442acf3
Sreejith R <rsreejith090@gmail.com>
2025-08-12 18:21:02 +0530

    String reverse question solving

---
+++ Day4/StringReverse.js
@@ -0,0 +1,12 @@
+//Reverse a String Without Using reverse() Method
+function reverseString() {
+    let input = "hello"
+    let reversedString =""
+    intoArray = input.split('')
+    for(i=intoArray[0]; i<=intoArray.length-1; i++){
+        console.log(i)
+    }
+    
+    // console.log(reversedString)
+}
+reverseString()
