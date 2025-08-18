f29bd8fef375b1c9010c20e751a406462f1ca37a
Sreejith R <rsreejith090@gmail.com>
2025-08-18 17:42:26 +0530

    sedcond largest number in an array problem completed

--- Day6/secondLargestDigit.js
+++ Day6/secondLargestDigit.js
@@ -1,4 +1,15 @@
-//Write a function in JavaScript that finds the second largest number in an array without using sort() or Math.max() twice.
+//Write a function in JavaScript that finds the second largest number in an array without using Math.max() twice.
 
 let input= [10, 5, 8, 20, 15]
+let largest = 0
+let secondLargest = 0;
+
+function findSecondLargest(){
+   
+    const sorted = input.sort((a,b)=>a-b)
+   length = sorted.length-2
+   secondLargest = sorted[3]
+    console.log(`the second largest number is ${secondLargest}`)
+}
+findSecondLargest()
       
