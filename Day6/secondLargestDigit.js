e618891f946470a19d5a87a6988e5083d4a074c9
Sreejith R <rsreejith090@gmail.com>
2025-09-06 22:46:32 +0530

    Second largest digit problem completed

--- Day6/secondLargestDigit.js
+++ Day6/secondLargestDigit.js
@@ -1,7 +1,6 @@
 //Write a function in JavaScript that finds the second largest number in an array without using Math.max() twice.
 
 let input= [10, 5, 8, 20, 15]
-let largest = 0
 let secondLargest = 0;
 
 function findSecondLargest(){
---
+++ Day7/nonRepeatingDigit.js
@@ -0,0 +1,18 @@
+//Find the first non-repeating element in the array.
+
+function nonRepeatingDigit(){
+       let array = [1,3,1,4,5,4,5]
+
+       for(i=0; i<array.length; i++){
+        count = 0;
+        for(j=0; i<array.length; j++){
+           
+            }
+        } if(array[i] === array[j]){
+            count++
+       } if(array[i]===1){
+        console.log(`the first non repeaiting digit is ${array[i]}`)
+       } 
+}
+
+nonRepeatingDigit()
